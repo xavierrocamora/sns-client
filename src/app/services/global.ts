46c0e372d569cd303de6732e7d71e4e88347d1e5
Xavier Rocamora <janzerker@hotmail.com>
2020-12-09 14:41:45 +0100

    fixed url

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,4 @@
 export const GLOBAL = {
-    url: 'http://localhost:3000/api/v1/'
-    //url: 'https://sns-serverside.herokuapp.com/api/v1/'
+    //url: 'http://localhost:3000/api/v1/'
+    url: 'https://sns-serverside.herokuapp.com/api/v1/'
 }
